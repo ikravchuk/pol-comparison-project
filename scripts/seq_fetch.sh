2e0cb294f7a4cec6154dec1e22a03ae0c7d6289f
Ihor Kravchuk <ihor.kravchuk.mail@gmail.com>
2017-09-11 13:40:33 +0300

    nseqs -> nseq_links, gitignore fna.gz faa.gz

--- scripts/seq_fetch.sh
+++ scripts/seq_fetch.sh
@@ -9,10 +9,10 @@ while IFS= read -r -d $'\0' file; do
 	CDS_SEQ_SUFFIX="_cds_from_genomic.fna.gz"
 	PROT_SEQ_SUFFIX="_protein.faa.gz"
 	
-	awk 'BEGIN{FS=OFS="/";filesuffix="cds_from_genomic.fna.gz"}{ftpdir=$0;asm=$10;file=asm"_"filesuffix;print ftpdir,file}' $file > ${DIRNAME}/nseqs 
+	awk 'BEGIN{FS=OFS="/";filesuffix="cds_from_genomic.fna.gz"}{ftpdir=$0;asm=$10;file=asm"_"filesuffix;print ftpdir,file}' $file > ${DIRNAME}/nseq_links 
        cat ${DIRNAME}/nseqs | parallel -j 20 --verbose --progress "cd ${DIRNAME}/nseq && curl -O {}"	
 	
-       awk 'BEGIN{FS=OFS="/";filesuffix="protein.faa.gz"}{ftpdir=$0;asm=$10;file=asm"_"filesuffix;print ftpdir,file}' $file > ${DIRNAME}/pseqs 
+       awk 'BEGIN{FS=OFS="/";filesuffix="protein.faa.gz"}{ftpdir=$0;asm=$10;file=asm"_"filesuffix;print ftpdir,file}' $file > ${DIRNAME}/pseq_links 
        cat ${DIRNAME}/pseqs | parallel -j 20 --verbose --progress "cd ${DIRNAME}/pseq && curl -O {}"	
 
 done < <(find */*paths -type f -print0)
