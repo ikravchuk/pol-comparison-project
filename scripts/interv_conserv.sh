5d84bcce17e9139d0c964993a6bc4a9fa9816399
Ihor Kravchuk <ihor.kravchuk.mail@gmail.com>
2017-09-15 19:59:03 +0300

    interv_conserv.sh generates matrix for .align files

--- scripts/interv_conserv.sh
+++ scripts/interv_conserv.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+while IFS= read -r -d $'\0' file; do
+	lines=( $(awk '/*/ {print FNR}' $file) )
+	for (( i=0; i<${#lines[@]}; i++ )); do cat $file | sed -n ${lines[i]}p | tail -c +17 | head -c -4 >> ${file}.matrix; done
+	
 # align (after mafft) 
 # cat n_pols.fna.align | sed -n '15p' | tail -c +17 | head -c -4
 # 3 lines + 11 (number of fastas) + 1 = first
@@ -7,4 +13,4 @@
 # clustal (after clustalo) 
 # cat n_pols.fna.clustal | sed -n '15p' | tail -c +45 | head -c -4
 
-
+done < <(find */*.align -type f -print0)
